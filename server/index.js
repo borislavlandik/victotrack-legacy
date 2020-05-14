226f8fba53f7ff8b1a263c8cf8a44fe6943daade
Борислав Ландик <borislavlandik00@gmail.com>
2020-05-14 16:21:04 +0800

    build(server): add express server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('test message')
+})
+
+app.listen(PORT, 'localhost', () => {
+    console.log(`Server started at ${PORT}`)
+})
