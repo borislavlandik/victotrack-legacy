1daaf1eacfe23c5ab37cbd726864e14c181acc94
Борислав Ландик <borislavlandik00@gmail.com>
2020-05-17 00:09:17 +0800

    feat(spotify): add method to get playlist tracks

--- client/src/services/spotify.js
+++ client/src/services/spotify.js
@@ -21,5 +21,31 @@ export default {
                     image: images[0] ? images[0].url : placeholder
                 }
             })
+    },
+    async getPlaylistTracks (playlistId) {
+        if (playlistId === undefined) {
+            throw new Error('Playlist ID is required')
+        }
+
+        const token = Cookies.get('user_token')
+
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(data => data.json())
+
+        return response.items
+            .map(item => {
+                const artists = item.track.artists.map(artist => artist.name)
+                const trackName = item.track.name
+
+                return {
+                    trackName: trackName,
+                    artists: artists,
+                    preview: item.track.preview_url,
+                    fullName: `${artists[0]} — ${trackName}`
+                }
+            })
     }
 }
